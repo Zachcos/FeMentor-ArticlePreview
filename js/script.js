7bf8abfb5606b7f1efe0d798ed93a9ce8bc93d2a
Zach Cossman <Zachcos@gmail.com>
2020-09-28 17:15:39 -0400

    Add share bubble

--- js/script.js
+++ js/script.js
@@ -10,7 +10,9 @@ var toggleShare = function() {
 var handleShare = function() {
   if (state.working) {
     document.querySelector("#share_btn").classList.add('active');
+    document.querySelector("#share_panel").classList.add('active');
   } else {
     document.querySelector("#share_btn").classList.remove('active')
+    document.querySelector("#share_panel").classList.remove('active')
   }
 }
